d88d6e9db2602cc072708c57f8f072a5335aa28d
sardor <madalievsardor33@gmail.com>
2025-02-01 20:48:30 +0500

    WORKERS

--- src/Pages/Users/Workers/Workers.jsx
+++ src/Pages/Users/Workers/Workers.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
-
-const Workers = () => {
-  return (
-    <div>
-      Workers
-    </div>
-  )
-}
-
-export default Workers
+import React, { useEffect, useState } from 'react'; 
+import { GridComponent, ColumnsDirective, ColumnDirective,Page, Search, Inject, Toolbar } from '@syncfusion/ej2-react-grids'; 
+ 
+import {employeesGrid } from '../../../Data/dummy'; 
+import Header from '../../../Components/Header'; 
+const Workers = () => { 
+  const [worker, setWorker] = useState([]) 
+ 
+  const requestWorker = async () => { 
+    try { 
+      const request = await fetch('https://iticket-reference-backend.onrender.com/api/v1/users/get-by-role?role=admin', { 
+        method: 'GET', 
+        headers: { 
+          'Content-Type': 'application/json', 
+          'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OTBiMmUwZDU0YTBlYjFjNjlkZjYwNiIsImVtYWlsIjoiYWRtaW5AZXhhbXBsZS5jb20iLCJpYXQiOjE3MzgyMzkwNzIsImV4cCI6MTczODg0Mzg3Mn0.1q0lKZDKWGn2zvjCv78asI0tx-SHEqyzE6mkMKfDjtU`, 
+        }, 
+      }); 
+      const response = await request.json(); 
+      setWorker(response || []);  
+    } catch (e) { 
+      console.error('Error fetching users:', e); 
+    } 
+  } 
+  useEffect(() => { 
+    requestWorker() 
+  }, []) 
+ 
+  return ( 
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl"> 
+      <Header category="Page" title="Employees" /> 
+      <GridComponent 
+        dataSource={worker} 
+        allowPaging 
+        allowSorting 
+        toolbar={['Search']} 
+        width= 'auto' 
+      > 
+        <ColumnsDirective> 
+          {/* Ustun konfiguratsiyasi */} 
+          <ColumnDirective field="_id" headerText="ID" width="150" textAlign="Center" /> 
+          <ColumnDirective field="firstName" headerText="First Name" width="130" textAlign="Center" /> 
+          <ColumnDirective field="lastName" headerText="Last Name" width="130" textAlign="Center" /> 
+          <ColumnDirective field="email" headerText="Email" width="140" textAlign="Center" /> 
+          <ColumnDirective field="phoneNumber" headerText="Phone Number" width="150" textAlign="Center" />
+          <ColumnDirective field="role" headerText="Role" width="110" textAlign="Center" /> 
+        </ColumnsDirective> 
+        <Inject services={[Page, Search, Toolbar]} /> 
+      </GridComponent> 
+    </div> 
+  ); 
+}; 
+export default Workers;
