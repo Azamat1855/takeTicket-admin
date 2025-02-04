26ff27460dff4752219adf4de436aadf5067476f
madalievsardor <madalievsardor33@gmail.com>
2025-02-04 18:15:28 +0500

    WORKERS

--- src/Pages/Users/Workers/Workers.jsx
+++ src/Pages/Users/Workers/Workers.jsx
@@ -25,7 +25,7 @@ const Workers = () => {
   }, []) 
  
   return ( 
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl"> 
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-base-100 rounded-3xl"> 
       <Header title="Worker" /> 
       <GridComponent 
         dataSource={worker} 
